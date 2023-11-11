54c2cc496f621103487b8136d48b88596faf40c5
Tamas Levai <levait@tmit.bme.hu>
2023-11-11 19:36:34 +0100

    XDP offload: Add first working tests (WIP) [skip ci]
    TODOs:
    - extend existing tests with detailed assertions (e.g., convert
    byte-array to proto.ChannelData)
    - add more tests (e.g., try different packet lengths?)

---
+++ internal/offload/xdp_test.go
@@ -0,0 +1,144 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+//go:build offloadxdp
+
+package offload
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"github.com/pion/logging"
+	"github.com/pion/turn/v3/internal/proto"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestXDPOffload executes XDP offload unit tests on localhost
+//
+// The tests include forwarding clients ChannelData traffic to peer, and peer UDP traffic to a client.
+//
+// The test setup on localhost exercising the 'lo' interface:
+// client (port 2000) <-> turn listen (3478) <- XDP offload -> turn relay (4000) <-> peer (5000)
+//
+// The loopback interface tends to generate incorrect UDP checksums, which is fine in most cases.
+// However, the XDP offload incrementally updates this checksum resulting errnous checksum on the
+// receiver side. This leads to packet drops. To mitigate this issue, tests use raw sockets to send
+// pre-crafted packets having correct checksums.
+//
+// Note: Running these tests require privileged users (e.g., root) to run the XDP offload.
+func TestXDPOffload(t *testing.T) {
+	// timeout of connnection read deadlines
+	const readTimeoutInterval = 500 * time.Millisecond
+
+	clientAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2000")
+	assert.NoError(t, err)
+	turnListenAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:3478")
+	assert.NoError(t, err)
+	turnRelayAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:4000")
+	assert.NoError(t, err)
+	peerAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:5000")
+	assert.NoError(t, err)
+
+	// IP addrs for raw sockets
+	clientIPAddr, err := net.ResolveIPAddr("ip4", "127.0.0.1")
+	assert.NoError(t, err)
+	turnListenIPAddr, err := net.ResolveIPAddr("ip4", "127.0.0.1")
+	assert.NoError(t, err)
+	turnRelayIPAddr, err := net.ResolveIPAddr("ip4", "127.0.0.1")
+	assert.NoError(t, err)
+	peerIPAddr, err := net.ResolveIPAddr("ip4", "127.0.0.1")
+	assert.NoError(t, err)
+
+	// UDP header + UDP Data ('payload')
+	udpSent := []byte{0x13, 0x88, 0x0f, 0xa0, 0x00, 0x0f, 0x21,
+		0x76, 0x70, 0x61, 0x79, 0x6c, 0x6f, 0x61, 0x64}
+
+	// UDP data consisting of the string 'payload'
+	udpRecv := []byte{0x70, 0x61, 0x79, 0x6c, 0x6f, 0x61, 0x64}
+
+	// UDP header + ChannelData header + 'payload' + padding
+	chanDataSent := []byte{0x07, 0xd0, 0x0d, 0x96, 0x00, 0x14,
+		0xef, 0x26, 0x40, 0x00, 0x00, 0x07, 0x70, 0x61, 0x79, 0x6c,
+		0x6f, 0x61, 0x64, 0x00}
+
+	// ChannelData header + 'payload' + padding
+	chanDataRecv := []byte{0x40, 0x00, 0x00, 0x07, 0x70, 0x61, 0x79,
+		0x6c, 0x6f, 0x61, 0x64, 0x00}
+
+	// setup client side
+	clientIPConn, err := net.ListenIP("ip4:udp", clientIPAddr)
+	assert.NoError(t, err, "cannot create client connection")
+	defer clientIPConn.Close()
+
+	// setup peer side
+	peerIPConn, err := net.ListenIP("ip4:udp", peerIPAddr)
+	assert.NoError(t, err, "cannot create peer connection")
+	defer peerIPConn.Close()
+
+	clientConn, err := net.ListenUDP("udp", clientAddr)
+	assert.NoError(t, err, "cannot create client connection")
+	defer clientConn.Close()
+
+	peerConn, err := net.ListenUDP("udp", peerAddr)
+	assert.NoError(t, err, "cannot create peer connection")
+	defer peerConn.Close()
+
+	// init offload
+	client := Connection{
+		LocalAddr:  turnListenAddr,
+		RemoteAddr: clientAddr,
+		Protocol:   proto.ProtoUDP,
+		ChannelID:  0x4000,
+	}
+	peer := Connection{
+		LocalAddr:  turnRelayAddr,
+		RemoteAddr: peerAddr,
+		Protocol:   proto.ProtoUDP,
+	}
+
+	loggerFactory := logging.NewDefaultLoggerFactory()
+	logger := loggerFactory.NewLogger("xdp-test")
+	lo, err := net.InterfaceByName("lo")
+	assert.NoError(t, err, "no interface: 'lo'")
+
+	xdpEngine, err := NewXdpEngine([]net.Interface{*lo}, logger)
+	assert.NoError(t, err, "cannot instantiate XDP offload engine")
+	defer xdpEngine.Shutdown()
+
+	err = xdpEngine.Init()
+	assert.NoError(t, err, "cannot init XDP offload engine")
+
+	t.Run("pass packet from peer to client", func(t *testing.T) {
+		xdpEngine.Upsert(client, peer)
+
+		received := make([]byte, len(chanDataRecv))
+
+		_, err := peerIPConn.WriteToIP(udpSent, turnRelayIPAddr)
+		assert.NoError(t, err, "error in sending peer data")
+
+		err = clientConn.SetReadDeadline(time.Now().Add(readTimeoutInterval))
+		assert.NoError(t, err)
+		_, _, err = clientConn.ReadFromUDP(received)
+		assert.NoError(t, err, "error in receiving data on client side")
+
+		assert.Equal(t, chanDataRecv, received, "expect match")
+	})
+
+	t.Run("pass packet from client to peer", func(t *testing.T) {
+		xdpEngine.Upsert(client, peer)
+
+		received := make([]byte, len(udpRecv))
+
+		_, err := clientIPConn.WriteToIP(chanDataSent, turnListenIPAddr)
+		assert.NoError(t, err, "error in sending channel data")
+
+		err = peerConn.SetReadDeadline(time.Now().Add(readTimeoutInterval))
+		assert.NoError(t, err)
+		_, _, err = peerConn.ReadFromUDP(received)
+		assert.NoError(t, err, "error in receiving data on client side")
+
+		assert.Equal(t, udpRecv, received, "expect match")
+	})
+}
