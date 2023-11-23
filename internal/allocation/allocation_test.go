f310c1db0e4707fdf09fb616335477b975b78859
Tamas Levai <levait@tmit.bme.hu>
2023-11-23 14:47:20 +0100

    Fix failing allocation tests

--- internal/allocation/allocation_test.go
+++ internal/allocation/allocation_test.go
@@ -19,6 +19,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var testFiveTuple = FiveTuple{
+	SrcAddr: &net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 2000},
+	DstAddr: &net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 4000},
+}
+
 func TestAllocation(t *testing.T) {
 	tt := []struct {
 		name string
@@ -46,7 +51,7 @@ func TestAllocation(t *testing.T) {
 }
 
 func subTestGetPermission(t *testing.T) {
-	a := NewAllocation(nil, nil, nil)
+	a := NewAllocation(nil, &testFiveTuple, nil)
 
 	addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:3478")
 	if err != nil {
@@ -88,7 +93,7 @@ func subTestGetPermission(t *testing.T) {
 }
 
 func subTestAddPermission(t *testing.T) {
-	a := NewAllocation(nil, nil, nil)
+	a := NewAllocation(nil, &testFiveTuple, nil)
 
 	addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:3478")
 	if err != nil {
@@ -107,7 +112,7 @@ func subTestAddPermission(t *testing.T) {
 }
 
 func subTestRemovePermission(t *testing.T) {
-	a := NewAllocation(nil, nil, nil)
+	a := NewAllocation(nil, &testFiveTuple, nil)
 
 	addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:3478")
 	if err != nil {
@@ -130,7 +135,7 @@ func subTestRemovePermission(t *testing.T) {
 }
 
 func subTestAddChannelBind(t *testing.T) {
-	a := NewAllocation(nil, nil, nil)
+	a := NewAllocation(nil, &testFiveTuple, nil)
 
 	addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:3478")
 	if err != nil {
@@ -154,7 +159,7 @@ func subTestAddChannelBind(t *testing.T) {
 }
 
 func subTestGetChannelByNumber(t *testing.T) {
-	a := NewAllocation(nil, nil, nil)
+	a := NewAllocation(nil, &testFiveTuple, nil)
 
 	addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:3478")
 	if err != nil {
@@ -173,7 +178,7 @@ func subTestGetChannelByNumber(t *testing.T) {
 }
 
 func subTestGetChannelByAddr(t *testing.T) {
-	a := NewAllocation(nil, nil, nil)
+	a := NewAllocation(nil, &testFiveTuple, nil)
 
 	addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:3478")
 	if err != nil {
@@ -193,7 +198,7 @@ func subTestGetChannelByAddr(t *testing.T) {
 }
 
 func subTestRemoveChannelBind(t *testing.T) {
-	a := NewAllocation(nil, nil, nil)
+	a := NewAllocation(nil, &testFiveTuple, nil)
 
 	addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:3478")
 	if err != nil {
@@ -214,7 +219,7 @@ func subTestRemoveChannelBind(t *testing.T) {
 }
 
 func subTestAllocationRefresh(t *testing.T) {
-	a := NewAllocation(nil, nil, nil)
+	a := NewAllocation(nil, &testFiveTuple, nil)
 
 	var wg sync.WaitGroup
 	wg.Add(1)
@@ -236,7 +241,7 @@ func subTestAllocationClose(t *testing.T) {
 		panic(err)
 	}
 
-	a := NewAllocation(nil, nil, nil)
+	a := NewAllocation(nil, &testFiveTuple, nil)
 	a.RelaySocket = l
 	// Add mock lifetimeTimer
 	a.lifetimeTimer = time.AfterFunc(proto.DefaultLifetime, func() {})
@@ -357,7 +362,7 @@ func subTestPacketHandler(t *testing.T) {
 }
 
 func subTestResponseCache(t *testing.T) {
-	a := NewAllocation(nil, nil, nil)
+	a := NewAllocation(nil, &testFiveTuple, nil)
 	transactionID := [stun.TransactionIDSize]byte{1, 2, 3}
 	responseAttrs := []stun.Setter{
 		&proto.Lifetime{
